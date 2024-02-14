79583b4ec02f0757efba58b28d57fb5e32c6af36
gmn1009@163.com <gmn1009@163.com>
2024-02-14 11:04:46 +0800

    assignment2

--- Assignment2.R
+++ Assignment2.R
@@ -1,22 +1,24 @@
 library(readr)
 library(dplyr)
-library(stringr)
 
-# 定义函数读取文件并提取指定列
-read_and_extract <- function(file_path) {
-  read_csv(file_path, col_types = cols()) %>%
-    select(doi, year = publication_year, journal, title) %>%
-    mutate(doi = str_to_upper(doi), journal = str_to_upper(journal), title = str_to_upper(title))
+# 列出当前工作目录中的所有.csv文件
+files_list <- list.files(pattern = "\\.csv$", full.names = TRUE)
+
+# 初始化一个空的数据框（如果直接使用bind_rows在data_list上，这一步可选）
+df <- data.frame()
+
+# 循环遍历文件并读取数据
+for (file_path in files_list) {
+  file_data <- read_csv(file_path)
+  df <- bind_rows(df, file_data) # 累积数据
 }
 
-# 修改后的主函数，直接处理当前工作目录中的CSV文件
-process_files_in_current_directory <- function() {
-  files_list <- list.files(pattern = "\\.csv$", full.names = TRUE)
-  combined_data <- lapply(files_list, read_and_extract) %>%
-    bind_rows()
-  return(combined_data)
-}
+# 可选，查看数据框的前几行
+print(head(df))
 
-# 执行修改后的主函数并打印结果
-combined_data <- process_files_in_current_directory()
-print(head(combined_data))
+# 使用lapply读取文件，然后合并它们
+data_list <- lapply(files_list, read_csv)
+combined_data <- bind_rows(data_list) # 直接合并，改名为combined_data
+
+
+
